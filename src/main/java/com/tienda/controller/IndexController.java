117c291cfd2c5d3fcfbf019a44a17975f78cc18e
JamesH2R <98919741+JamesH2R@users.noreply.github.com>
2022-07-11 23:51:29 -0600

    Semana 8 Tarea 3 del CRUD

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,34 @@
+
+package com.tienda.controller;
+
+
+import com.tienda.service.ArticuloService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Slf4j
+
+@Controller 
+public class IndexController {
+    @Autowired
+    private ArticuloService articuloService;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+       
+        log.info("Estamos del lado de programacion");
+        
+        var articulos = articuloService.getArticulos(true);
+        
+        model.addAttribute("articulos",articulos);
+        return "index";
+    }
+    
+    
+}
---
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -0,0 +1,56 @@
+
+package com.tienda.domain;
+
+import java.io.Serializable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="cliente")
+
+public class Cliente implements Serializable {
+    
+    private static final long serialVersionUID = 1l;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    
+    private Long idCliente;
+    String nombre;
+    String apellidos;
+    String correo;
+    String telefono;
+    
+    @JoinColumn(name="id_credito",referencedColumnName = "id_credito")
+    @ManyToOne
+    private Credito credito;
+   
+
+    public Cliente() {
+    }
+
+    public Cliente(String nombre, String apellidos, String correo, String telefono) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Cliente(String nombre, String apellidos, String correo, String telefono, Credito credito) {
+       
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.credito = credito;
+    }
+            
+    
+}
